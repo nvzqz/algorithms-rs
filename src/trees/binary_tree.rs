0ce1f3d7b449cbde3459ea427aaa409b33c1cf14
Nikolai Vazquez <nvazquez1297@gmail.com>
2017-12-09 21:46:07 -0500

    Rename data to value in BinaryTree

--- src/trees/binary_tree.rs
+++ src/trees/binary_tree.rs
@@ -1,22 +1,22 @@
 /// A binary tree.
 pub struct BinaryTree<T> {
-    data: T,
+    value: T,
     left: Option<Box<BinaryTree<T>>>,
     right: Option<Box<BinaryTree<T>>>,
 }
 
 impl<T> From<T> for BinaryTree<T> {
     #[inline]
-    fn from(val: T) -> BinaryTree<T> {
-        BinaryTree { data: val, left: None, right: None }
+    fn from(value: T) -> BinaryTree<T> {
+        BinaryTree { value: value, left: None, right: None }
     }
 }
 
 impl<T> BinaryTree<T> {
     /// Returns the data owned by this node.
     #[inline]
-    pub fn data(&self) -> &T {
-        &self.data
+    pub fn value(&self) -> &T {
+        &self.value
     }
 
     /// Returns the left binary tree node.
